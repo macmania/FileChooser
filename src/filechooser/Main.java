ef6bf551e86f09820b78155704689fbd0a8e7bd5
Jouella Fabe <jojofabe@vt.edu>
2014-01-11 23:18:08 -0500

    fixed loading file functionality

--- src/filechooser/Main.java
+++ src/filechooser/Main.java
@@ -6,10 +6,17 @@
 package filechooser;
 
 
+import java.io.File;
+import java.util.List;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 /**
@@ -17,16 +24,28 @@ import javafx.stage.Stage;
  * @author jojofabe
  */
 public class Main extends Application {
-    
+    @FXML Button loadFile; 
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
+        FileChooser fileChooser = new FileChooser(); 
+        loadFile.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(final ActionEvent t) {
+                List<File> list = fileChooser.showOpenMultipleDialog(stage);
+                if(list != null){
+                    System.out.println("opened file"); 
+                }
+            }
+        });
+        
         
         stage.setScene(scene);
         
-            stage.show();
+        stage.show();
        
     }
 
