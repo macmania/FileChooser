723785e7dc4d467b3da10931f7394b283e6429d9
jojofabe <jojofabe@vt.edu>
2014-01-12 12:58:03 -0500

    before

--- src/Models/CheckBoxCellModel.java
+++ src/Models/CheckBoxCellModel.java
@@ -6,23 +6,30 @@
 
 package Models;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
 
 public class CheckBoxCellModel extends TableCell<DataModel, Boolean> {
-       @FXML private CheckBox checkBox ;
-        
+       @FXML private CheckBoxCellModel checkBox;
+       
+        boolean empty; 
         public CheckBoxCellModel() {
-            checkBox = new CheckBox() ;
+            checkBox = new CheckBoxCellModel();
             checkBox.setAlignment(Pos.CENTER);
             
             this.setGraphic(checkBox);
             this.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
             this.setEditable(true);
         }
+        
         @Override
         public void startEdit() {
             super.startEdit();
@@ -32,24 +39,39 @@ public class CheckBoxCellModel extends TableCell<DataModel, Boolean> {
             checkBox.setDisable(false);
             checkBox.requestFocus();
         }
+        
         @Override
         public void cancelEdit() {
             super.cancelEdit();
             checkBox.setDisable(true);
         }
-        public void commitEdit(Boolean value) {
-            super.commitEdit(value);
-            checkBox.setDisable(true);
-        }
+        
+         @FXML TableView tableView;
         @Override
         public void updateItem(Boolean item, boolean empty) {
             super.updateItem(item, empty);
+            this.empty = empty; 
             if (!isEmpty()) {
-                checkBox.setSelected(item);
+                
+                checkBox.setOnMouseClicked(new EventHandler<MouseEvent>(){
+
+                    @Override
+                    public void handle(MouseEvent t) {
+                        
+                        CheckBox cb = (CheckBox) t.getSource();
+                        TableColumn column = (TableColumn)cb.getUserData();
+                       
+                        if(cb.isSelected()) { 
+                            System.out.println(tableView.toString());
+                            System.out.println("selected"); }
+                        else System.out.println("not selected"); 
+                    }
+                });
                 setGraphic(checkBox);
             }
             else{
                 setGraphic(null);
+               
             }
-        }
-    }
+        }   
+ }
